b4ca3e1a6c3daf3d9eb5717a876ca82637b1480f
yanoshingo <yanoshingo119@gmail.com>
2021-11-15 11:45:14 +0900

    ここからhtml経由でデータのやり取りができるか試してみる

--- js/Firebase.js
+++ js/Firebase.js
@@ -19,12 +19,11 @@ const firebaseConfig = {
     appId: "1:278552571035:web:8517296195d194fb6b6a3e"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const dbRef = ref(db, "Youtube-info");
-
-
 
 const auth = getAuth();
 signInAnonymously(auth)
---
+++ js/main.js
@@ -0,0 +1,140 @@
+
+//****************************************************************************************
+// BingMaps&BmapQuery マップのjQueryの部分
+//****************************************************************************************
+//Init
+function GetMap() {
+    //------------------------------------------------------------------------
+    //1. Instance
+    //------------------------------------------------------------------------
+    const map = new Bmap("#myMap");
+    //------------------------------------------------------------------------
+    //2. Display Map
+    //   startMap(lat, lon, "MapType", Zoom[1~20]);
+    //   MapType:[load, aerial,canvasDark,canvasLight,birdseye,grayscale,streetside]
+    //--------------------------------------------------
+    map.startMap(35.712772, 139.750443, "canvasLight", 10);
+
+    // キーワード検索で座標を取ってきて、その座標を表示
+    // map.getGeocode("Seattle", function (data) {
+    //     console.log(data);          //Get Geocode ObjectData
+    //     const lat = data.latitude;  //Get latitude
+    //     const lon = data.longitude; //Get longitude
+    //     document.querySelector("#geocode").innerHTML = lat + '<br>' + lon;
+    // });
+
+    //----------------------------------------------------
+    //3. Add Pushpin-Icon 
+    // pinIcon(lat, lon, icon, scale, anchor_x, anchor_y);
+    //----------------------------------------------------
+    //let pin = map.pinIcon(47.6130, -122.1945, "../img/poi_custom.png", 1.0, 0, 0);
+
+    //クリックすると座標を取ってくる
+    map.onGeocode("click", function (data) {
+        //console.log(data);                   //Get Geocode ObjectData
+        const lat = data.location.latitude;  //Get latitude
+        const lon = data.location.longitude; //Get longitude
+        console.log(lat + ':' + lon);
+        let pin1 = map.infobox(lat, lon, "この場所に動画をマッピングしますか？", "設定画面に移動");
+        //map.pinLayerClear(pin2);
+        //map.pinIcon(lat, lon, "BmapQuery-master/img/poi_custom.png", 1.0, 12, 39);
+    });
+
+    //現在地表示
+    map.geolocation(function (data) {
+        //location
+        const lat = data.coords.latitude;
+        const lon = data.coords.longitude;
+        //Map
+        // map.startMap(lat, lon, "load", 10);
+        //pin
+        map.pin(lat, lon, "#ff0000");
+    });
+
+
+
+}
+
+// ここまでがマップのjQueryの部分
+
+
+
+
+
+let mX = 0; //マウスのX軸位置情報をグローバル変数へ保存
+let mY = 0; //マウスのY軸位置情報をグローバル変数へ保存
+window.onload = function () {
+    //マウス移動時のイベントをBODYタグに登録する
+    document.body.addEventListener("mousemove", function (e) {
+        //座標を取得する
+        mX = e.pageX;  //X座標
+        mY = e.pageY;  //Y座標
+        return [mX, mY];
+    });
+}
+
+// 左のメニューアイテムにカーソルを乗せると案内が出る
+$('.menu-item').mouseover(function () {
+    $(this).children(".description").css("left", mX + 10);
+    $(this).children(".description").css("top", mY);
+    $(this).children(".description").show();
+});
+// 左のメニューアイテムにカーソルが外れると案内が消える
+$('.menu-item').mouseout(hideDescription);
+
+//検索バーのアイコンにカーソルを乗せると案内が出る
+$('.search-icon').mouseover(showDescription);
+
+//検索バーのアイコンからカーソルが外れると案内が消える
+$('.search-icon').mouseout(hideDescription);
+
+//検索バーのアイコンにカーソルを乗せると案内が出る
+$('.youtube-icon').mouseover(showDescription);
+
+//検索バーのアイコンからカーソルが外れると案内が消える
+$('.youtube-icon').mouseout(hideDescription);
+
+//検索バーのアイコンにカーソルを乗せると案内が出る
+$('.insta-icon').mouseover(showDescription);
+
+//検索バーのアイコンからカーソルが外れると案内が消える
+$('.insta-icon').mouseout(hideDescription);
+
+//経路検索アイコンにカーソルを乗せると案内が出る
+$('.go-there-icon').mouseover(showDescription);
+
+//経路検索アイコンからカーソルが外れると案内が消える
+$('.go-there-icon').mouseout(hideDescription);
+
+//デスクリプションクラスを隠す関数
+function hideDescription() {
+    $(this).children(".description").hide();
+}
+// デスクリプションクラスを出す関数
+function showDescription() {
+    $(this).children(".description").show();
+}
+
+$(function () { //オープニング画面エフェクト
+    $(".top h1").addClass("is-fadein");
+    $(".top").addClass("is-fadein");
+    setTimeout(function () {
+        $(".top h1").css("display", "none");
+        $(".top").css("display", "none");
+    }, 5500);
+});
+
+$(function () { //メッセージ画面移管
+    setTimeout(function () {
+        $(".top-announce").css("display", "flex");
+        $(".top-announce").css("justify-content", "center");
+        $(".top-announce").css("align-items", "center");
+    }, 5500);
+});
+
+$(function () { //メイン画面に移管
+    setTimeout(function () {
+        $(".top-announce").css("display", "none");
+        $(".main").css("display", "flex");
+    }, 8500);
+});
