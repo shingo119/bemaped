fab431a5ec4dc60d23e880943feac3c07e322fdf
yanoshingo <yanoshingo119@gmail.com>
2021-11-18 03:39:21 +0900

    擬似的にデータをローカルストレージに番号を振って保存

--- js/Firebase.js
+++ js/Firebase.js
@@ -3,7 +3,7 @@
 // 必要なJSを読み込み
 //###############################################
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
-import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved }from "https://www.gstatic.com/firebasejs/9.3.0/firebase-database.js";
+import { getDatabase, ref, push, set, query, child, startAt, get, onChildAdded, remove, onChildRemoved }from "https://www.gstatic.com/firebasejs/9.3.0/firebase-database.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -80,34 +80,6 @@ onAuthStateChanged(auth, (user) => {
             
             // uidをローカルストレージへ入れようとしている
             localStorage.setItem("uid", uid);
-
-            
-
-            // $("#submit").on("click", function () {
-            //     const movieTitle = document.querySelector("#movie-title").value;
-            //     const movieUrl = document.querySelector("#movie-url").value;
-            //     const tag = document.querySelector("#tag").value;
-            //     const ifram = document.querySelector("#ifram").value;
-            //     const lat = sessionStorage.getItem('lat');
-            //     const lon = sessionStorage.getItem('lon');
-            //     const uid = localStorage.getItem('uid');
-
-            //     const msg = {
-            //         movieTitle: movieTitle,
-            //         movieUrl: movieUrl,
-            //         tag: tag,
-            //         ifram: ifram,
-            //         lat: lat,
-            //         lon: lon,
-            //         uid: uid
-            //     }
-            //     console.log(msg);//ちゃんとオブジェクトが吐かれる
-            //     const newPostRef = push(dbRef);
-            //     set(newPostRef, msg);
-            //     if (confirm('ページ遷移しますか？')) {
-            //         window.location.href = 'index.html';
-            //     }
-            // });
         }
     } else {
         // _redirect();  // User is signed out
@@ -150,11 +122,6 @@ function _redirect() {
 //     });
 
 
-// ログイン中の挙動に転記してみる↓
-
-// const db = getDatabase(app);
-// const dbRef = ref(db, "Youtube-info");
-
 $(document).on("click", "#submit", function () {
     const movieTitle = document.querySelector("#movie-title").value;
     const movieUrl = document.querySelector("#movie-url").value;
@@ -182,29 +149,66 @@ $(document).on("click", "#submit", function () {
 });
 
 //最初にデータ取得＆onSnapshotでリアルタイムにデータを取得
-// onChildAdded(dbRef, function (data) {
-//     const obj = data.val();    //オブジェクトデータを取得し、変数msgに代入
-//     const key = data.key;      //データのユニークキー（削除や更新に使用可能）
-// //     //表示用テキスト・HTMLを作成
-//     let movieOut = `<div class='movie-output-list' id='${key}'>`;
-//     movieOut += `<p>`;
-//     movieOut += obj.movieTitle;
-//     movieOut += `</p><p>`;
-//     movieOut += obj.movieUrl;
-//     movieOut += `</p><p>`;
-//     movieOut += obj.tag;
-//     movieOut += `</p><p class="ifram">`;
-//     movieOut += obj.ifram;
-//     movieOut += `</p><p class="uid">`;
-//     movieOut += obj.uid;
-//     movieOut += `</p><p class="lat">`;
-//     movieOut += obj.lat;
-//     movieOut += `</p><p class="lon">`;
-//     movieOut += obj.lon;
-//     movieOut += '</p></div>';
-//     $("#movie-date").append(movieOut); //#outputの最後に追加
-//     //console.log(obj);
-// });
+//onChildAdded(dbRef, function (data) {
+    //const obj = data.val();    //オブジェクトデータを取得し、変数msgに代入
+   // const key = data.key;      //データのユニークキー（削除や更新に使用可能）
+    //表示用テキスト・HTMLを作成
+    // let movieOut = `<div class='movie-output-list' id='${key}'>`;
+    // movieOut += `<p class="movieTitle">`;
+    // movieOut += obj.movieTitle;
+    // movieOut += `</p><p class="movieUrl">`;
+    // movieOut += obj.movieUrl;
+    // movieOut += `</p><p class="tag">`;
+    // movieOut += obj.tag;
+    // movieOut += `</p><p class="ifram">`;
+    // movieOut += obj.ifram;
+    // movieOut += `</p><p class="uid">`;
+    // movieOut += obj.uid;
+    // movieOut += `</p><p class="lat">`;
+    // movieOut += obj.lat;
+    // movieOut += `</p><p class="lon">`;
+    // movieOut += obj.lon;
+    // movieOut += '</p></div>';
+    //$("#movie-date").append(movieOut); //#outputの最後に追加
+    //console.log(obj);
+//});
 
+//データベースから引っ張ることは無理
+// const movieTitle = query(ref(db, 'Youtube-info'), startAt('温泉'));
+// console.log(movieTitle);
+
+//function wordSearch (word){
+    //movieTitleクラスのエレメントリスト作成する
+    //const movieTitle = document.getElementsByClassName('movieTitle');
+    //console.log(movieTitle);
+    //エレメントリストの各エレメントをfor文で全て確認
+    // for(let i; i < movieTitle.length; i++){
+    //     //エレメントのテキストが部分一致している場合のif文
+    //     console.log(~movieTitle[i].innerHTML.indexOf(word));
+    //     if (movieTitle[i].innerHTML.indexOf(word) !== -1){
+    //         //親要素を取得
+    //         const parentElement = movieTitle[i].parentNode;
+    //         //親要素のクラスを取得
+    //         const parentClassName = parentElement.className;
+    //         //.親要素クラス名 .子要素クラス名でquerycelecter調べてみる
+    //         const movieUrl = document.querySelector('.'+parentClassName+' .movieUrl');
+    //         const tag = document.querySelector('.'+parentClassName+' .tag');
+    //         const ifram = document.querySelector('.'+parentClassName+' .ifram');
+    //         const uid = document.querySelector('.' + parentClassName +' .uid');
+    //         const lat = document.querySelector('.' + parentClassName +' .lat');
+    //         const lon = document.querySelector('.' + parentClassName +' .lon');
+    //     }
+    // }
+//}
+
+let num = 0;
+onChildAdded(dbRef, function(data){
+    num++
+    const obj = data.val();
+    const key = data.key;
+    //console.log(obj);
+    const str = JSON.stringify(obj);
+    localStorage.setItem(num,str);
+});
 
 
